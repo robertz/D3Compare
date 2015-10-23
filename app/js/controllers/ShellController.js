23396a64fdcd742b95b603275fc3fcef87614999
Robert Zehnder <robert@kisdigital.com>
2015-10-22 21:55:01 -0400

    Initial commit

---
+++ app/js/controllers/ShellController.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+  angular
+    .module('app')
+    .controller('ShellController', ShellController);
+
+  ShellController.$inject = ['$rootScope', '$scope', 'BattleNetService'];
+
+  function ShellController($rootScope, $scope, BattleNetService) {
+    var vm = this;
+
+    vm.leftAccountInput = 'mordhel-1221';
+    vm.rightAccountInput = 'cfjedimaster-1136';
+    vm.lefSelect = '';
+    vm.rightSelect = '';
+    vm.leftAccount = {};
+    vm.rightAccount = {};
+    vm.leftCharacterData = {};
+    vm.rightCharacterData = {};
+    vm.getLeftAccount = getLeftAccount;
+    vm.getRightAccount = getRightAccount;
+    vm.getLeftHero = getLeftHero;
+    vm.getRightHero = getRightHero;
+
+    vm.leftActive = false;
+    vm.rightActive = false;
+
+    function getLeftAccount() {
+      BattleNetService
+        .getAccount(vm.leftAccountInput)
+        .then(function(data) {
+          vm.leftAccount = data;
+        });
+    }
+
+    function getRightAccount() {
+      BattleNetService
+        .getAccount(vm.rightAccountInput)
+        .then(function(data) {
+          vm.rightAccount = data;
+        });
+    }
+
+    function getLeftHero() {
+      BattleNetService
+        .getHero(vm.leftAccountInput, vm.leftSelect.id)
+        .then(function(data) {
+          vm.leftActive = true;
+          vm.leftCharacterData = data;
+        });
+    }
+
+    function getRightHero() {
+      BattleNetService
+        .getHero(vm.rightAccountInput, vm.rightSelect.id)
+        .then(function(data) {
+          vm.rightActive = true;
+          vm.rightCharacterData = data;
+        });
+    }
+  }
+})();
---
+++ app/js/services/BattleNetService.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+  angular
+    .module('app')
+    .service('BattleNetService', BattleNetService);
+
+  BattleNetService.$inject = ['$http'];
+
+  function BattleNetService($http) {
+    return {
+      getAccount: getBNetAccount,
+      getHero   : getBNetHero
+    };
+
+    function getBNetAccount(account) {
+      return $http.get('https://us.api.battle.net/d3/profile/' + account + '/?locale=en_US&apiKey=3q8vurf6jnh85bs54hn4q9skp3h4r2g2')
+        .then(function(response) {
+          return response.data;
+        });
+    }
+
+    function getBNetHero(account, id) {
+      return $http.get('https://us.api.battle.net/d3/profile/' + account + '/hero/' + id + '?locale=en_US&apiKey=3q8vurf6jnh85bs54hn4q9skp3h4r2g2')
+        .then(function(response) {
+          return response.data;
+        });
+    }
+  }
+})();
