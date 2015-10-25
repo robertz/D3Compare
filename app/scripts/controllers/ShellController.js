5247f7839b1ec7e782756716552fd1a5b1d91cf8
Robert Zehnder <robert@kisdigital.com>
2015-10-24 23:39:41 -0400

    ngDiaglog hooks, refactoring shell and compare controllers

--- app/scripts/controllers/ShellController.js
+++ app/scripts/controllers/ShellController.js
@@ -4,59 +4,30 @@
     .module('app')
     .controller('ShellController', ShellController);
 
-  ShellController.$inject = ['$rootScope', '$scope', 'BattleNetService'];
+  ShellController.$inject = ['ngDialog'];
 
-  function ShellController($rootScope, $scope, BattleNetService) {
+  function ShellController(ngDialog) {
     var vm = this;
+    var aboutResult = null;
 
-    vm.leftAccountInput = 'mordhel-1221';
-    vm.rightAccountInput = 'cfjedimaster-1136';
-    vm.lefSelect = '';
-    vm.rightSelect = '';
-    vm.leftAccount = {};
-    vm.rightAccount = {};
-    vm.leftCharacterData = {};
-    vm.rightCharacterData = {};
-    vm.getLeftAccount = getLeftAccount;
-    vm.getRightAccount = getRightAccount;
-    vm.getLeftHero = getLeftHero;
-    vm.getRightHero = getRightHero;
-
-    vm.leftActive = false;
-    vm.rightActive = false;
-
-    function getLeftAccount() {
-      BattleNetService
-        .getAccount(vm.leftAccountInput)
-        .then(function(data) {
-          vm.leftAccount = data;
+    vm.showAbout = function(e) {
+      e.stopPropagation();
+      if (!_.isNull(aboutResult)) {
+        aboutResult.close();
+      } else {
+        ngDialog.closeAll();
+        aboutResult = ngDialog.open({
+          template: 'views/about.html',
+          overlay: true,
+          className: 'ngdialog-theme-default',
+          scope: $('body > div').scope()
         });
-    }
-
-    function getRightAccount() {
-      BattleNetService
-        .getAccount(vm.rightAccountInput)
-        .then(function(data) {
-          vm.rightAccount = data;
+        // $('body').addClass('about');
+        aboutResult.closePromise.then(function() {
+          aboutResult = null;
+          // $('body').removeClass('about');
         });
-    }
-
-    function getLeftHero() {
-      BattleNetService
-        .getHero(vm.leftAccountInput, vm.leftSelect.id)
-        .then(function(data) {
-          vm.leftActive = true;
-          vm.leftCharacterData = data;
-        });
-    }
-
-    function getRightHero() {
-      BattleNetService
-        .getHero(vm.rightAccountInput, vm.rightSelect.id)
-        .then(function(data) {
-          vm.rightActive = true;
-          vm.rightCharacterData = data;
-        });
-    }
+      }
+    };
   }
 })();
