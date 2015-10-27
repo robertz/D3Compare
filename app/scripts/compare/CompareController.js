8192d7e7757c150fe9981fa3c89564e0f40f2a32
Robert Zehnder <robert@kisdigital.com>
2015-10-26 21:57:37 -0400

    Diff values between characters

--- app/scripts/compare/CompareController.js
+++ app/scripts/compare/CompareController.js
@@ -22,6 +22,13 @@
     vm.getLeftHero = getLeftHero;
     vm.getRightHero = getRightHero;
 
+    // Calculate diffs for stat values
+    vm.stats = {
+      leftChar: {},
+      rightChar: {}
+    };
+
+    // Remap payload class variables
     vm.classLabel = {
       'barbarian': 'Barbarian',
       'crusader': 'Crusader',
@@ -34,6 +41,7 @@
     vm.leftActive = false;
     vm.rightActive = false;
 
+    // Show json debug block under character info?
     vm.debug = false;
 
     function getLeftAccount() {
@@ -58,6 +66,7 @@
         .then(function(data) {
           vm.leftActive = true;
           vm.leftCharacterData = data;
+          recalcStatDiffs();
         });
     }
 
@@ -67,7 +76,18 @@
         .then(function(data) {
           vm.rightActive = true;
           vm.rightCharacterData = data;
+          recalcStatDiffs();
         });
+    }
+
+    function recalcStatDiffs() {
+      if (!(vm.leftActive && vm.rightActive)) {
+        return;
+      }
+      for (var key in vm.leftCharacterData.stats) {
+        vm.stats.leftChar[key] = vm.leftCharacterData.stats[key] - vm.rightCharacterData.stats[key];
+        vm.stats.rightChar[key] = vm.rightCharacterData.stats[key] - vm.leftCharacterData.stats[key];
+      }
     }
   }
 })();
